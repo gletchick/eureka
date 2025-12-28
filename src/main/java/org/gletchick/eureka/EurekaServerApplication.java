77cc4214b7311b2127374022a9467f796b52372c
gletchick <magleb_vgv@mail.ru>
2025-12-28 23:49:15 +0300

    feat: add Eureka in app

--- src/main/java/org/gletchick/eureka/EurekaServerApplication.java
+++ src/main/java/org/gletchick/eureka/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package org.gletchick.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaApplication {
-
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
-
 }
